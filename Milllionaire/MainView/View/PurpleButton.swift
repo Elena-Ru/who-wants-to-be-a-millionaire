878152fee69c1c9e22b9763b76f864e8fa42591f
Elena <lenarusskih@gmail.com>
2023-05-23 14:42:07 +0300

    Add adoptive layout for Settings View using Size Classes. Add additional image for the logo for different Size classes.

--- Milllionaire/MainView/View/PurpleButton.swift
+++ Milllionaire/MainView/View/PurpleButton.swift
@@ -17,7 +17,6 @@ final class PurpleButton: UIButton {
     layer.backgroundColor = #colorLiteral(red: 0.2763207555, green: 0.1030554697, blue: 0.3165085614, alpha: 1)
     setTitle(title, for: .normal)
     setTitleColor(.white, for: .normal)
-//       titleLabel?.font = UIFont.boldSystemFont(ofSize: 20)
     layer.cornerRadius = 20
     layer.borderColor = UIColor.yellow.cgColor
     layer.borderWidth = 2.0
--- Milllionaire/Settings/View/SettingsRootView.swift
+++ Milllionaire/Settings/View/SettingsRootView.swift
@@ -8,14 +8,16 @@
 import UIKit
 
 
-final class SettingsRootView : UIView {
+final class SettingsRootView : UIView, SizeClassesCompitable {
   
   //MARK: Variables
   private let shuffleLabel: UILabel = {
     let label = UILabel()
     label.text = Texts.SettingsVC.shuffle
     label.textColor = .white
-    label.font = UIFont.systemFont(ofSize: 18)
+    label.lineBreakMode = .byWordWrapping
+    label.numberOfLines = 0
+    label.textAlignment = .natural
     label.translatesAutoresizingMaskIntoConstraints = false
     return label
   }()
@@ -25,6 +27,17 @@ final class SettingsRootView : UIView {
     let switchV = CustomSwitch(isOn: isOn)
     return switchV
   }()
+  
+  private let topContainerView: UIView = {
+    let view = UIView()
+    view.translatesAutoresizingMaskIntoConstraints = false
+    return view
+  }()
+  
+  var compactConstraints: [NSLayoutConstraint] = []
+  var regularConstraints: [NSLayoutConstraint] = []
+  var sharedConstraints: [NSLayoutConstraint] = []
+  var ipadConstraints: [NSLayoutConstraint] = []
   
   //MARK: Inits
   init() {
@@ -39,32 +52,65 @@ final class SettingsRootView : UIView {
   
   //MARK: Private methods
   private func creatView() {
-    setupLayout()
+    setupUI()
+    setupFont(traitCollection: traitCollection)
+    setupConstraints()
+    layoutTrait(traitCollection: traitCollection)
   }
   
-  private func setupLayout() {
-    let topContainerView = UIView()
-    self.addSubview(topContainerView)
+  private func setupUI() {
+    addSubview(topContainerView)
     topContainerView.addSubview(shuffleLabel)
     topContainerView.addSubview(shuffleSwitch)
-    topContainerView.translatesAutoresizingMaskIntoConstraints = false
-    
-    NSLayoutConstraint.activate([
+  }
+  
+  private func setupConstraints() {
+    setupSharedConstraints()
+    setupRegularCostaints()
+    setupCompactCostaints()
+  }
+  
+  private func setupSharedConstraints() {
+    sharedConstraints.append(contentsOf: [
       topContainerView.topAnchor.constraint(equalTo: self.safeAreaLayoutGuide.topAnchor),
       topContainerView.leadingAnchor.constraint(equalTo: self.safeAreaLayoutGuide.leadingAnchor),
       topContainerView.trailingAnchor.constraint(equalTo: self.safeAreaLayoutGuide.trailingAnchor),
       topContainerView.heightAnchor.constraint(equalTo: self.heightAnchor, multiplier: 0.1),
       
       shuffleLabel.topAnchor.constraint(equalTo: topContainerView.topAnchor, constant: 25),
-      shuffleLabel.leadingAnchor.constraint(equalTo: topContainerView.leadingAnchor, constant: 20),
       shuffleLabel.widthAnchor.constraint(equalTo: topContainerView.widthAnchor, multiplier: 0.7) ,
-      shuffleLabel.heightAnchor.constraint(equalToConstant: 30),
+      shuffleLabel.heightAnchor.constraint(equalToConstant: 60),
       
       shuffleSwitch.centerYAnchor.constraint(equalTo: shuffleLabel.centerYAnchor),
       shuffleSwitch.trailingAnchor.constraint(equalTo: topContainerView.trailingAnchor, constant: -40),
       shuffleSwitch.widthAnchor.constraint(equalToConstant: 50),
       shuffleSwitch.heightAnchor.constraint(equalToConstant: 30)
     ])
+  }
+  
+  private func setupRegularCostaints() {
+    regularConstraints.append(contentsOf: [
+      shuffleLabel.trailingAnchor.constraint(equalTo: shuffleSwitch.leadingAnchor, constant: 20)
+    ])
+  }
+  
+  private func setupCompactCostaints() {
+    compactConstraints.append(contentsOf: [
+      shuffleLabel.leadingAnchor.constraint(equalTo: topContainerView.leadingAnchor, constant: 20)
+    ])
+  }
+  
+  func setupFont(traitCollection: UITraitCollection) {
+    switch traitCollection.horizontalSizeClass {
+    case .compact:
+      shuffleLabel.font = UIFont.systemFont(ofSize: Constants.compactFontSize)
+    case .regular:
+      shuffleLabel.font = UIFont.systemFont(ofSize: Constants.regularFontSize)
+    case .unspecified:
+      break
+    @unknown default:
+      fatalError()
+    }
   }
 }
 
