9c2dd483c89c2d1399a25162e84974d31d5649a1
Elena <lenarusskih@gmail.com>
2023-09-11 09:28:23 +0300

    Add extension for UIColor with color scheme

---
+++ Milllionaire/Extnsions/UIColor.swift
@@ -0,0 +1,20 @@
+//
+//  UIColor.swift
+//  Milllionaire
+//
+//  Created by Елена Русских on 2023-09-11.
+//
+
+import UIKit
+
+extension UIColor {
+    static let millionaireBackground: UIColor = UIColor(red: 0/255, green: 0/255, blue: 34/255, alpha: 1.0)
+    static let millionaireText: UIColor = UIColor(red: 255/255, green: 255/255, blue: 255/255, alpha: 1.0)
+    static let millionaireSilver: UIColor = UIColor(red: 192/255, green: 192/255, blue: 192/255, alpha: 1.0)
+    static let millionaireGold: UIColor = UIColor(red: 255/255, green: 215/255, blue: 0/255, alpha: 1.0)
+    static let millionaireHighlight: UIColor = UIColor(red: 0/255, green: 102/255, blue: 255/255, alpha: 1.0)
+    static let millionaireError: UIColor = UIColor(red: 255/255, green: 0/255, blue: 0/255, alpha: 1.0)
+    static let millionaireSuccess: UIColor = UIColor(red: 0/255, green: 255/255, blue: 0/255, alpha: 1.0)
+    static let millionaireSecondary: UIColor = UIColor(red: 68/255, green: 68/255, blue: 68/255, alpha: 1.0)
+}
+
