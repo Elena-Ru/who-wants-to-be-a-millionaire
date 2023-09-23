ed2330350597cd676a2fa5acd7aba117e7ac0ae7
Elena <lenarusskih@gmail.com>
2023-09-23 16:15:52 +0300

    Refactr Setting Screen Add setting for color scheme

--- Milllionaire/Settings/View/SettingsRootView.swift
+++ Milllionaire/Settings/View/SettingsRootView.swift
@@ -8,75 +8,31 @@
 import UIKit
 import SnapKit
 
-
-final class SettingsRootView : UIView {
+final class SettingsRootView: UIView {
     
-    let shuffleLabel: UILabel = {
-        let label = UILabel()
-        label.text = Texts.shuffle
-        label.textColor = .white
-        label.font = UIFont.systemFont(ofSize: 18)
-        label.translatesAutoresizingMaskIntoConstraints = false
-        return label
-    }()
+    let tableView: UITableView
     
-    
-    var shuffleSwitch: CustomSwitch = {
-        var isOn = setPosition()
-        let switchV = CustomSwitch(isOn: isOn)
-        return switchV
-    }()
-    
-     init() {
-        super.init(frame: CGRect())
-        creatView()
+    override init(frame: CGRect) {
+        tableView = UITableView(frame: .zero, style: .grouped)
+        tableView.backgroundColor = UIColor(named: "background")
+        super.init(frame: frame)
+        
+        setupView()
     }
     
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
     
-    private func creatView() {
-        setupLayout()
-    }
-    
-    private func setupLayout() {
-        let topContainerView = UIView()
-        self.addSubview(topContainerView)
-        topContainerView.addSubview(shuffleLabel)
-        topContainerView.addSubview(shuffleSwitch)
-        topContainerView.translatesAutoresizingMaskIntoConstraints = false
-      
-        topContainerView.snp.makeConstraints { (make) -> Void in
+    private func setupView() {
+        addSubview(tableView)
+        tableView.translatesAutoresizingMaskIntoConstraints = false
         
-          make.top.equalTo(self.snp_topMargin)
-          make.leading.equalTo(self.snp.leading)
-          make.trailing.equalTo(self.snp.trailing)
-          make.height.equalTo(self.snp.height).multipliedBy(0.1)
-        }
-        
-        shuffleLabel.snp.makeConstraints { (make) -> Void in
-        
-          make.top.equalTo(topContainerView.snp.top).offset(25)
-          make.leading.equalTo(topContainerView.snp.leading)  .offset(20)
-          make.width.equalTo(topContainerView.snp.width)  .multipliedBy(0.7)
-          make.height.equalTo(20)
-        }
-      
-        shuffleSwitch.snp.makeConstraints { (make) -> Void in
-        
-          make.centerY.equalTo(shuffleLabel.snp.centerY)
-          make.trailing.equalTo(topContainerView.snp.trailing)  .offset(-40)
-          make.width.equalTo(50)
-          make.height.equalTo(30)
-        }
-    }
-}
-
-fileprivate  func setPosition() -> Bool {
-    if Game.shared.selectedDifficulty == .easy {
-        return false
-    } else {
-        return true
+        NSLayoutConstraint.activate([
+            tableView.topAnchor.constraint(equalTo: topAnchor),
+            tableView.bottomAnchor.constraint(equalTo: bottomAnchor),
+            tableView.leftAnchor.constraint(equalTo: leftAnchor),
+            tableView.rightAnchor.constraint(equalTo: rightAnchor)
+        ])
     }
 }
