55080a76163d13a50983f6e8c4fb52d29b725a15
Elena <lenarusskih@gmail.com>
2023-09-25 07:49:57 +0300

    Refactor GameVC

--- Milllionaire/Game/Controller/GameViewController.swift
+++ Milllionaire/Game/Controller/GameViewController.swift
@@ -12,7 +12,7 @@ protocol GameViewControllerDelegate: AnyObject {
     func didEndGame ( withResult result: Results)
 }
 
-class GameViewController: UIViewController {
+final class GameViewController: UIViewController {
 
     var currentResults = Game.shared
     var difficulty: Difficulty = Game.shared.selectedDifficulty
@@ -106,49 +106,66 @@ extension GameViewController: UITableViewDelegate, UITableViewDataSource {
     func tableView(_ tableView: UITableView, viewForFooterInSection section: Int) -> UIView? {
         UIView()
     }
-    
+  
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         tableView.deselectRow(at: indexPath, animated: true)
+      
         guard let question = currentQuestion else { return }
-        
-        let answer = question.answers[indexPath.section]
+        handleAnswerSelection(for: question, at: indexPath.section)
+    }
+    
+    private func handleAnswerSelection(for question: Question, at index: Int) {
+        let answer = question.answers[index]
+      
         if checkAnswer(answer: answer, question: question) {
-            
-            if let index = questionsWithStrategy.firstIndex(where: { $0.text == question.text}) {
-                
-                corAnswer += 1
-                
-                if index < questionsWithStrategy.count - 1 {
-                    let nextQuestion = questionsWithStrategy[index + 1]
-                    currentResults.session?.currentQuestionNumber.value += 1
-                    currentResults.session?.procent.value = currentResults.corAnswerProcent(correctAnswerCount: corAnswer, totalQuestionCount: questionsWithStrategy.count)
-                    configureUI(question: nextQuestion)
-                    tableView.reloadData()
-                } else {
-                    router.presentAlert(title: Texts.gameOver, message: Texts.congratulations) {
-                              self.router.closeGame()
-                              self.gameDelegate?.didEndGame(withResult: Results(procent: self.currentResults.session?.procent.value, correctAnswerCount: self.corAnswer))
-                          }
-                    currentResults.session?.procent.value = currentResults.corAnswerProcent(correctAnswerCount: corAnswer, totalQuestionCount: questionsWithStrategy.count)
-                    gameDelegate?.didEndGame(withResult: Results(procent: currentResults.session?.procent.value, correctAnswerCount: corAnswer))
-                    corAnswer = 0
-                    currentQuestion = questionsWithStrategy[0]
-                }
-            }
+          handleCorrectAnswer(for: question)
         } else {
-
-            router.presentAlert(title: Texts.wrong, message: Texts.tryAgain) {
-                      self.router.closeGame()
-                      self.gameDelegate?.didEndGame(withResult: Results(procent: self.currentResults.session?.procent.value, correctAnswerCount: self.corAnswer))
-                  }
-            gameDelegate?.didEndGame(withResult: Results(procent: currentResults.session?.procent.value, correctAnswerCount: corAnswer))
-            corAnswer = 0
-            currentQuestion = questionsWithStrategy[0]
+          handleIncorrectAnswer()
         }
+    }
+
+    private func handleCorrectAnswer(for question: Question) {
+        guard let index = questionsWithStrategy.firstIndex(where: { $0.text == question.text }) else { return }
+        
+        corAnswer += 1
+        if index < questionsWithStrategy.count - 1 {
+            goToNextQuestion(from: index)
+        } else {
+            finishGameSuccessfully()
+        }
+    }
+
+    private func goToNextQuestion(from index: Int) {
+        let nextQuestion = questionsWithStrategy[index + 1]
+        currentResults.session?.currentQuestionNumber.value += 1
+        currentResults.session?.procent.value = currentResults.corAnswerProcent(correctAnswerCount: corAnswer, totalQuestionCount: questionsWithStrategy.count)
+        configureUI(question: nextQuestion)
+        rootView.tableView.reloadData()
+    }
+  
+    private func finishGameSuccessfully() {
+        router.presentAlert(title: Texts.gameOver, message: Texts.congratulations) {
+            self.router.closeGame()
+            self.gameDelegate?.didEndGame(withResult: Results(procent: self.currentResults.session?.procent.value, correctAnswerCount: self.corAnswer))
+        }
+        currentResults.session?.procent.value = currentResults.corAnswerProcent(correctAnswerCount: corAnswer, totalQuestionCount: questionsWithStrategy.count)
+        gameDelegate?.didEndGame(withResult: Results(procent: currentResults.session?.procent.value, correctAnswerCount: corAnswer))
+        corAnswer = 0
+        currentQuestion = questionsWithStrategy[0]
+    }
+
+    private func handleIncorrectAnswer() {
+        router.presentAlert(title: Texts.wrong, message: Texts.tryAgain) {
+            self.router.closeGame()
+            self.gameDelegate?.didEndGame(withResult: Results(procent: self.currentResults.session?.procent.value, correctAnswerCount: self.corAnswer))
+        }
+        gameDelegate?.didEndGame(withResult: Results(procent: currentResults.session?.procent.value, correctAnswerCount: corAnswer))
+        corAnswer = 0
+        currentQuestion = questionsWithStrategy[0]
     }
   
     func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
-        return (tableView.frame.size.height - 30 ) / 4
+        (tableView.frame.size.height - 30 ) / 4
     }
     
 }
